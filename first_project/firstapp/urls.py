e5ebbd7da954151045ce6698f750c526f47a3702
idknow94 <i.dont.kei.94@gmail.com>
2021-08-09 11:59:36 +0400

    changed some things

--- first_project/firstapp/urls.py
+++ first_project/firstapp/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from firstapp import views
 
 urlpatterns = [
+    path('', views.my_name),
     path('hello/', views.hello),
     path('datetime/', views.daytime),
-    path('list_task', views.list_tasks),
-    path('', views.my_name),
+    path('list_task/', views.list_tasks),
+    path('dict/', views.ret_dict_square),
+    path('bye/', views.bye),
+    path('json/', views.rjson),
+    path('lol/', views.lol),
 ]
--- first_project/firstapp/views.py
+++ first_project/firstapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import HttpResponse
 from django.utils import timezone
 from .models import Task
+import json
+import os
 
 
 def hello(request):
@@ -9,12 +11,12 @@ def hello(request):
 
 def my_name(request):
     return HttpResponse(
-        """"<h1><u><i>My name is Alek Aleksanyan</i></u></h1>""")
+        """"<h1>My name is <i>MAINDOTPY</i></h1>""")
 
 
 def daytime(request):
     now = timezone.now()
-    return HttpResponse(f"<h1>%s/%s</h1>" % (now.date(), now))
+    return HttpResponse(f"<h1>%s/%s</h1>" % (now.date(), now.time()))
 
 
 def list_tasks(request):
@@ -25,3 +27,25 @@ def list_tasks(request):
     <h3>tasks: {tasks[1]}</32>
     """
     return HttpResponse(html)
+
+
+def ret_dict_square(request):
+    dct = {}
+    for i in range(1, 16):
+        dct[i] = i ** 2
+
+    return HttpResponse(f"""<h1>%s</h1>""" % dct)
+
+
+def bye(request):
+    return HttpResponse(f"""<h1>bye<h1>""")
+
+
+def rjson(request):
+    with open(os.path.join(os.getcwd(), 'firstapp', 'read.json'), 'r') as f:
+        j = json.load(f)
+    return HttpResponse(f"""<h1>{j}</h1>""")
+
+
+def lol(request):
+    return HttpResponse(f"""<h1>LOL</h1>""")
